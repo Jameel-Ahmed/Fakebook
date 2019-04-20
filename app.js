3e0c3b0d41b308df599eb8818acbef06851db981
Jameel Ahmed <jydpak@gmail.com>
2019-04-20 13:32:27 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -25,49 +25,48 @@ $(function () {
     var helper = Helper();
 
     var i;
-   
-        helper.Call('https://jsonplaceholder.typicode.com/posts', 'GET', {}, function (response) {
 
-            helper.Call('https://jsonplaceholder.typicode.com/photos', 'GET', {}, function (result) {
+    helper.Call('https://jsonplaceholder.typicode.com/posts', 'GET', {}, function (response) {
 
-                helper.Call('https://jsonplaceholder.typicode.com/comments', 'GET', {}, function (comments) {
-    
-                var template = '';
-    
-                    $.each(response,function(i,item){
-                    var img  = result.filter(function(image){
-                        return image.id === item.id;
-                    });
+        // helper.Call('https://jsonplaceholder.typicode.com/photos', 'GET', {}, function (result) {
 
-                    var comment  = comments.filter(function(cmnt){
+        helper.Call('https://jsonplaceholder.typicode.com/comments', 'GET', {}, function (comments) {
+
+            var template = '';
+            var dd = response.filter(function (r) {
+                return r.id < 10;
+            });
+            $.each(dd, function (i, item) {
+                helper.Call('http://www.splashbase.co/api/v1/images/random', 'GET', {}, function (rimg) {
+
+                    //console.log(rimg);
+                    var comment = comments.filter(function (cmnt) {
                         return cmnt.postId === item.id;
                     });
-                    var imgurl = img[0].url;
+                    var imgurl = rimg.url;
                     template += `
-                    <div class="card" style="width:100%">
-                        <img class="card-img-top" height="200px" src="${imgurl}">
-                        <div class="card-body">
-                            <h4 class="card-title"></h4>
-                            <article class="card-text">${item.body}</article>`;
-                            
-                                template += `<li class="list-group-item .list-group-flush">${comment[0].body.substring(0,30) + '...'}</li>`;
-                                
-                           
-                 template += `<input type="text" class="form-control" placeholder="comment something ...">
-                        </div>
-                    </div>
-                    `;
+                            <div class="card" style="width:100%">
+                                <img class="card-img-top" height="200px" src="${imgurl}">
+                                <div class="card-body">
+                                    <h4 class="card-title"></h4>
+                                    <p class="card-text" style="font-size:16px;">${item.body.substring(0, 50) + '...'}</p>`;
+
+                    //template += `<li class="list-group-item .list-group-flush">${comment[0].body.substring(0,30) + '...'}</li>`;
+
+                    template += `<input type="text" class="form-control" placeholder="comment something ...">
+                                </div>
+                            </div>
+                            `;
+                            //console.log(template);
+                            $('#app').html(template);
                 });
-    
-                $('#app').html(template);
-
-                $('article').readmore();
 
             });
-    
-            });
-    
+
+          //  $('#app').html(template);
         });
+
+    });
 });
 
 
